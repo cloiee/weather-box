77fa60052cf17a8936c9d7cb5f6cb37f162c4730
cloiee <70982086+cloiee@users.noreply.github.com>
2020-11-16 11:25:26 +0000

    added capitalisation for text

--- script.js
+++ script.js
@@ -51,8 +51,9 @@ searchForm.addEventListener("click", handleSubmit);
 function displayWeather(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
 
   let descriptionElement = document.querySelector("#description");
